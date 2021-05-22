7bf3f0bd9543a095f0d5967336ffc58c0e734cf6
nobinsjohns <nobinsjohns1@gmail.com>
2021-05-22 19:57:14 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <h3>Counter</h3>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>{" "}
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <div className="NavBar">
+        <h2 className="Net">COUNTER</h2>
+      </div>
+      <div className="Body">
+        <h3 className="Head">COUNTER</h3>
+        <h1 className="Count">{count}</h1>
+        <div className="BtnGrp">
+        <button className ="Button" onClick={() => setCount(count + 1)}>+</button>{" "}
+        <button className="Button" onClick={() => setCount(count - 1)}>-</button>
+        </div>
+      </div>
     </div>
   );
 }
